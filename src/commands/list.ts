e8563fd630c537b9e05545e3a03423b1b48b8fbf
Anders Kok <ak.kornerup@gmail.com>
2021-01-05 22:20:33 +0100

    Updated list command

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -2,28 +2,38 @@ import chalk from "chalk";
 import { injectable } from "tsyringe";
 import { Colors } from "../constants/colors";
 import { TaskService } from "../services/task.service";
-import { CliCommand } from "./command.interface";
+import { CliCommand, IOption } from "./command.interface";
 
 @injectable()
 export class ListCommand extends CliCommand {
     name = ['list', 'overview', 'all'];
+    options: IOption[] = [{ synopsis: '-p, --procent', description: 'Display number in procent' }]
     description = 'Quick view over the tasks';
 
     constructor(
         private taskService: TaskService
     ) { super(); }
 
-    action(params) {
+    action({options}) {
+        let categorysToShow = { a: 'A', b: 'B', c: 'C', missing: 'Missing', g: 'Genafleverings' };
+
         let assignments = this.taskService.currentAssignments;
         let longestKapitel = assignments.reduce((acc, cur) => acc < cur.kapitel.length ? cur.kapitel.length : acc, 0) + 2;
         let assignmentsToPrint: string[] = ['\n'];
         assignmentsToPrint = assignments.map(category => {
-            return `${category.kapitel.padEnd(longestKapitel, ' ')}|  ` +
-                chalk[Colors.a]`A: ${('' + (category.a || '').length).padEnd(3, ' ')}  ` +
-                chalk[Colors.b]`B: ${('' + (category.b || '').length).padEnd(3, ' ')}  ` +
-                chalk[Colors.c]`C: ${('' + (category.c || '').length).padEnd(3, ' ')}  ` +
-                chalk[Colors.missing]`Manglende: ${('' + (category.missing || '').length).padEnd(3, ' ')}  ` +
-                chalk[Colors.g]`Genaflevering: ${('' + (category.g || '').length).padEnd(3, ' ')}  `
+            let totalAssignments = Object.keys(categorysToShow).reduce((acc, group) => {
+                return acc + (category[group]?.length || 0);
+            }, 0);
+
+            let prettyAssignments: string[] = Object.entries(categorysToShow).map(([key, label]) => {
+                let number: string = options.procent ? (totalAssignments / category[key]?.length * 100) : category[key]?.length;
+                number = number || '0';
+                number += options.procent ? '%' : '';
+
+                return chalk[Colors[key]]`${label}: ${number.padEnd(4, ' ')}`
+            });
+
+            return `${category.kapitel.padEnd(longestKapitel, ' ')}|  ` + prettyAssignments.join('  ');
         })
 
         assignmentsToPrint.unshift('');
